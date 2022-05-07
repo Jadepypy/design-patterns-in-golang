984370927ced2aaeadf67a3dbcbe8f813571dd96
Jade Chang <jade.chang@thecarousell.com>
2022-05-07 15:30:49 +0800

    add documentation

--- singleton/singleton.go
+++ singleton/singleton.go
@@ -1,15 +1,27 @@
+// Package singleton is a demonstration of singleton pattern
 package singleton
 
+import "fmt"
+
+// a boiler should be unique, since we only have one boiler to boil milk
 var uniqueInstance *boiler
 
 type boiler struct {
 	IsEmpty bool
 }
 
+// Fill fills up the boiler with milk
 func (b *boiler) Fill() {
-	b.IsEmpty = false
+	if b.IsEmpty {
+		b.IsEmpty = false
+	} else {
+		fmt.Println("Boiler is already filled!")
+	}
+
 }
 
+// GetInstance is the key function of singleton pattern
+// It returns a boiler instance and makes sure that there is only one existing instance.
 func GetInstance() *boiler {
 	if uniqueInstance == nil {
 		uniqueInstance = &boiler{true}
